e8ff1fb0e94c7477433c40e15f1062807196860c
MoamenAdel <moamenadel94@gmail.com>
2017-09-27 19:37:29 +0200

    resolve confilct errors

--- src/app/routing/app.nav.ts
+++ src/app/routing/app.nav.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router'
-import{AddProjectComponent} from'../component/addProject.component'import{AddProjectTypeComponent} from'../component/addProjectType.component'
-import{ViewProjectComponent} from '../component/ViewProject.component'import{ViewLFMComponent} from'../component/viewLFM.component';
+import{AddProjectComponent} from'../component/addProject.component'
+import{AddProjectTypeComponent} from'../component/addProjectType.component'
+import{ViewProjectComponent} from '../component/ViewProject.component'
+import{ViewLFMComponent} from'../component/viewLFM.component';
 import{AddLFMComponent} from'../component/addLFM.component';const APP_ROUTE: Routes = [
     {path:'',component:AddProjectComponent},
  {path:'addProjectType',component:AddProjectTypeComponent},
-    {path:'viewProject',component:ViewProjectComponent}    {path:'viewLFM',component:ViewLFMComponent},
+    {path:'viewProject',component:ViewProjectComponent}  ,  {path:'viewLFM',component:ViewLFMComponent},
     {path:'addLFM',component:AddLFMComponent}    
     
 ];
